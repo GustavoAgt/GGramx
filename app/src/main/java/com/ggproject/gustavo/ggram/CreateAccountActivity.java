04f2a060f7ae56a64ca9894b3df34544f80c2e01
GustavoAgt <gustavo.adolf.gomez@gmail.com>
2017-04-01 18:04:33 -0400

    "Completing LoginActivity and adding CreateAccountActivity"

--- app/src/main/java/com/ggproject/gustavo/ggram/CreateAccountActivity.java
+++ app/src/main/java/com/ggproject/gustavo/ggram/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.ggproject.gustavo.ggram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolBar(getResources().getString(R.string.toolbar_tittle_createaccount), false);
+    }
+
+    public void showToolBar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }

a38b38b643eb7874bb36bd8ba293fb78efbbec9a
GustavoAgt <gustavo.adolf.gomez@gmail.com>
2017-04-23 09:20:35 -0400

    complete detail activity and layout

--- app/src/main/java/com/ggproject/gustavo/ggram/view/PictureDetailActivity.java
+++ app/src/main/java/com/ggproject/gustavo/ggram/view/PictureDetailActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.ggproject.gustavo.ggram.R;
 
-public class PictureDatailActivity extends AppCompatActivity {
+public class PictureDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_picture_datail);
+        setContentView(R.layout.activity_picture_detail);
     }
 }
